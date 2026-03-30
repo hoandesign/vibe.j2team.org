1ef3eb23de19ed18c83e6ed9a7ebef0fbcf6c3f4
MTDES23 <thachminhtri23@gmail.com>
2026-03-31 03:55:24 +0700

    feat: add github-stars sub-app (#427)

---
+++ src/views/github-stars/meta.ts
@@ -0,0 +1,11 @@
+import type { PageMeta } from '@/types/page'
+
+const meta: PageMeta = {
+  name: 'Top GitHub Repos',
+  description: 'Bảng xếp hạng các dự án (repository) được gắn nhiều sao (star) nhất trên GitHub.',
+  author: 'mtdes23',
+  category: 'tool',
+  facebook: 'https://www.facebook.com/mtdes233/',
+}
+
+export default meta
